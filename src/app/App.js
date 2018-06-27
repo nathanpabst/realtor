ea035fd8451f38ec652dda131b9d0b481a7d6da7
Nathan Pabst <nathan.pabst@gmail.com>
2018-06-26 19:16:58 -0500

    added listing form

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
 import Buildings from '../components/Building/Building';
+import ListingForm from '../components/ListingForm/ListingForm';
 
 import './App.css';
 
@@ -9,8 +10,9 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-      <Listings />
-      <Buildings />
+        <Listings />
+        <Buildings />
+        <ListingForm />
       </div>
     );
   }
---
+++ src/components/ListingForm/ListingForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ListingForm.css';
+
+class ListingForm extends React.Component {
+  render () {
+    return (
+      <div className="ListingForm">
+        <h2>Listing Form</h2>
+      </div>
+    );
+  }
+}
+
+export default ListingForm;
